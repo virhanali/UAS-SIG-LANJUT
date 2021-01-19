ffa9a5a0321388bf1ce2f88b7c55f0de73539362
Virhan Ali Hameini <dev.virhan@gmail.com>
2021-01-19 18:16:25 +0700

    first commit

---
+++ denied.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UAS SIG LANJUT</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<h1>Time Limit Reached</h1>
+</body>
+</html>
